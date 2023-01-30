d19e73ce4fa58afb00a0b48db17d88a6bc9bce33
Hughost0 <hughost0@gmail.com>
2023-01-30 17:32:39 -0400

    pagina bien
    gaaa

--- main/app/patients/urls.py
+++ main/app/patients/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path 
 urlpatterns = [
 
-    path('', views.index, name="index"),
+    path('', views.index, name="index.html"),
     # path('patients/', views.patients, name="patients"),
     # path('services/', views.services, name="services"),
 ]
+    
